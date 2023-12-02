405a27739d9b048a36dd3a500d624ce1282db82a
Thomas DOUCHE <Thomas.douche@aol.com>
2023-12-02 19:30:50 +0100

    Multiplicaton and division support for durations

--- antlr/scripts/runGrun.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-#Execute chmod +x runGrun.sh to make the script executable
-
-source ./config.sh
-
-cd ../javaParser
-echo "Running grun..."
-java -cp ".:$ANTLR_JAR" org.antlr.v4.gui.TestRig $GRAMMAR_NAME $RULE_NAME -gui $INPUT_FILE
